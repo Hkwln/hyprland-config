06d90223187a818aca4c7befb56b49053ce424d0
Hkwln <p.aul@mail.de>
2026-04-28 13:03:11 +0200

    added submap to lua config

--- hypr/switch_to_lua/hyprland.lua
+++ hypr/switch_to_lua/hyprland.lua
@@ -2,7 +2,8 @@
 --MY NEW LUA HYPRLAND CONFIG
 -- TODO: 
 --]]
-
+---@diagnostic disable-next-line: undefined-global
+local hl = lh
 -----------------------MONITORS--------------------
 hl.monitor({
 	output = "eDP-1",
@@ -29,10 +30,32 @@ hl.config({})
 ---------------------Input--------------------------
 ---------------------Key Bindings-------------------
 ---launching apps:
-hl.bind("SUPER + T", hl.dsp.exec_cmd("firefox"))
+hl.bind("SUPER + B", hl.dsp.exec_cmd("zen-browser"))
+hl.bind("SUPER + T", hl.dsp.exec_cmd("kitty")({ description = "my favourite terminal" }))
+hl.bind("SUPER + SPACE", hl.dsp.exec_cmd("hyprlauncher"))
+hl.bind("SUPER +  Udiaeresis", hl.dsp.exec_cmd("hyprlock"))
 --maniputating windows:
+hl.bind("SUPER + C", hl.dsp.kill_active())
+hl.bind("SUPER + M", hl.dsp.exit())
 ---move focus:
 ---function keys:
+hl.bind("SUPER + ISO_Level3_Shift", hl.dsp.submap("mediamode"))
+hl.define_submap("mediamode", function()
+	hl.bind("F1", hl.dsp.exec_cmd("playerctl previous"))
+	hl.bind("F2", hl.dsp.exec_cmd("playerctl next"))
+	-- TODO: add F3
+	hl.bind("F3", hl.dsp.exec_cmd(""))
+	hl.bind("F4", hl.dsp.exec_cmd("killall -SIGUSR1 waybar || waybar &"))
+	-- TODO: add F5
+	hl.bind("F5", hl.dsp.exec_cmd(""))
+	hl.bind("F6", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"))
+	hl.bind("F7", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"))
+	hl.bind("F8", hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"))
+	hl.bind("F9", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"))
+	hl.bind("F10", hl.dsp.exec_cmd(" wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"))
+	hl.bind("N", hl.dsp.exec_cmd("pkill hyprsunset || hyprsunset -t 4000"))
+	hl.bind("ESCAPE", hl.dsp.submap("reset"))
+end)
 --------------------Windows and Workspaces----------
 ---[[ only activate if it doesn't work in the new lua config
 hl.window_rule({
